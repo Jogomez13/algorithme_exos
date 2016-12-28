671b5d91e548372d53f225f26fdc43ece5ec0e4d
Jogomez13 <jonathan.gomez.web@gmail.com>
2016-12-28 16:51:06 +0100

    exo6 en java + exo 4 en java et php

--- exo6/exo6.java
+++ exo6/exo6.java
@@ -5,7 +5,10 @@ public class exo6{
 //appel du fichier compile
 	public static void main(String[] args){
 		boolean flag = false;
+//on initialise un flag a false
+
 		while(flag == false){
+//tant que flage egale false
 		Scanner sc = new Scanner(System.in);
 //on creer un nouvel objet de type Scanner
 		System.out.println("Veuillez saisir un mot :");
@@ -16,14 +19,19 @@ public class exo6{
 
 //Sortie standard qui permet d'afficher la phrase ci-dessus
 			Scanner clavier = new Scanner(System.in);
+//on creer un nouvel objet de type Scanner : clavier
+			boolean quittez = false;
+			while(quittez==false){
 			System.out.print("\n Voulez vous quittez (o ou n)? ");
             		char reponse = clavier.next().charAt(0);
             		if (reponse == 'o' || reponse == 'O') {
+//si la reponse est egale a o ou O on quitte le programme
                 		System.exit(0);
-				}
-		
+				}else if (reponse =='n' || reponse == 'N') {
+			quittez = true;
+		}else
+		System.out.print("Hicham n'a pas compris Voulez vous quittez (o ou n)? ");
 		}
-		
+	}
 	}
 }
-
